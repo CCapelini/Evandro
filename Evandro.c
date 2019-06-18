3a4a7c92d123fb188568a9e2c0ef10eb5d1ba0a1
NicholasConner <43212753+NicholasConner@users.noreply.github.com>
2019-06-18 10:35:50 -0300

    Update Evandro.c

--- Evandro.c
+++ Evandro.c
@@ -1,5 +1,9 @@
 /*Nome: Bruno Rossi Carmo N: 11236289	
   Nome: Carlos Eduardo Capellini N: 11280140*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //Biblioteca de strings
+
 deu certo? consegue ver minhas alterações?
 struct
